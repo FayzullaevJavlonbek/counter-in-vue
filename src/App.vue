d3d32f3fe78384e973c6069e911108fa7bd97e42
Javlonbek <fayzullaevjavlonbek.d@gmail.com>
2023-03-04 19:38:38 +0500

    counter

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h1>Counter: {{ counter }}</h1>
+        <button v-on:click="Add" class="btn btn-success">Add</button>
+        <button @click="Delete" class="btn btn-danger">Delete</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            counter: 0,
+        }
+    },
+    methods: {
+        Add(){
+            this.counter +=1
+        },
+        Delete(){
+            this.counter -=1
+        }
+    }
+}
+</script>
+
+<style></style>
